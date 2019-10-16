86bdb52227ed5ed6713b279f5043880f1632ffe8
PAL-1 <purushottam_b_kumbha@dell.com>
2019-10-16 12:14:33 +0530

    Add Db transaction code

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+
 
 namespace PalTracker
 {
@@ -38,8 +40,12 @@ namespace PalTracker
                     Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR not configured.")                    
                 ));
 
-            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
